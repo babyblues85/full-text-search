b1ea569f8f43dad443611eebd28fa6db2fabc88d
Piotrek Okoński <piotrek@okonski.org>
2014-07-23 00:56:32 +0200

    Initiali keyword highlighting in search results.

--- app/helpers/things_helper.rb
+++ app/helpers/things_helper.rb
@@ -1,2 +1,13 @@
 module ThingsHelper
+
+  def highlighted_content(content, query)
+    stems = Thing.prepare_words(query)[:included]
+    words = Thing.word_breaker_value.new(content).split
+
+    conversions = Hash[words.map { |word| [Thing.stemmer_value.stem(word), word] }].slice(*stems)
+    conversions.values.each do |word|
+      content.gsub!(word) { |highlight| content_tag(:span, highlight, class: 'highlight') }
+    end
+    content
+  end
 end
