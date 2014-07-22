0a0e10254212e1750aad7725000b299378be875c
Piotrek Okoński <piotrek@okonski.org>
2014-07-22 23:02:27 +0200

    Refactor searching to store stems in SearchDocument#stems postgres array.
    This greatly simplifies the query and makes it easy to refine results by excluded words.

--- app/models/concerns/searchable.rb
+++ app/models/concerns/searchable.rb
@@ -3,7 +3,7 @@ module Searchable
 
   included do
     class_attribute :searchable_columns_value
-    has_many :stems, as: :searchable
+    has_many :search_documents, as: :searchable
     after_save :update_stems
   end
 
@@ -12,22 +12,17 @@ module Searchable
       raise "Define searchable columns using #searchable_columns class method"
     end
 
-    Stem.transaction do
-      Stem.where(searchable_type: self.class.name, searchable_id: self.id)
-          .delete_all
+    SearchDocument.transaction do
+      document = SearchDocument.find_or_initialize_by(searchable_type: self.class.name, searchable_id: self.id)
 
       words = self.attributes.symbolize_keys
               .values_at(*Array(self.searchable_columns_value))
               .flat_map { |value| DefaultWordBreaker.new(value).split }
+              .uniq
+              .map { |word| DefaultStemmer.stem(word) }
 
-      stems = []
-
-      words.uniq.each do |word|
-        stem = DefaultStemmer.stem(word)
-        stems << Stem.new(searchable_type: self.class.name, searchable_id: self.id, word: stem)
-      end
-
-      Stem.import stems
+      document.stems = words
+      document.save
     end
   end
 
@@ -38,45 +33,40 @@ module Searchable
 
     def search(query, options = {})
       prepared_words = prepare_words(query)
-      regulars   = prepared_words[:regular]
       inclusions = prepared_words[:included]
       exclusions = prepared_words[:excluded]
 
-      sql = "id IN ("
-      sql += "#{stems_query(regulars)}" if regulars.any?
-      sql += " INTERSECT #{stems_query(inclusions)}" if inclusions.any?
-      sql += " EXCEPT #{stems_query(exclusions)}" if exclusions.any?
-      sql += ")"
-
-      where(sql)
+      where("id IN (#{stems_query(inclusions, exclusions)})")
     end
 
     def prepare_words(query)
       words = query.split(/ +/)
       results = {
-        regular: [],
         excluded: [],
         included: []
       }
 
       words.each do |word|
-        case word.first
-        when "-"
+        if word.first == "-"
           results[:excluded] << DefaultStemmer.stem(word[1..-1])
-        when "+"
-          results[:included] << DefaultStemmer.stem(word[1..-1])
         else
-          results[:regular] << DefaultStemmer.stem(word)
+          results[:included] << DefaultStemmer.stem(word)
         end
       end
 
       results
     end
 
-    def stems_query(words)
-      Stem.select(:searchable_id)
-          .where(searchable_type: 'Thing', word: words)
-          .to_sql
+    def stems_query(inclusions, exclusions)
+      relation = SearchDocument.select(:searchable_id)
+          .where(searchable_type: 'Thing')
+          .where("stems @> ARRAY[?]::varchar[]", inclusions)
+
+      if exclusions.any?
+        relation = relation.where.not("stems && ARRAY[?]::varchar[]", exclusions)
+      end
+
+      relation.to_sql
     end
   end
 end
