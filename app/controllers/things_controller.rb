5fff7524c8ad070fdb3927ec0d038f01dccdd25c
Piotrek Okoński <piotrek@okonski.org>
2014-07-23 01:03:12 +0200

    Add action for creating new things.

--- app/controllers/things_controller.rb
+++ app/controllers/things_controller.rb
@@ -11,4 +11,14 @@ class ThingsController < ApplicationController
   def new
     @thing = Thing.new
   end
+
+  def create
+    @thing = Thing.new
+    @thing.content = params[:thing][:content]
+    if @thing.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 end
