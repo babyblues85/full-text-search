7161d172bb2a85e6d721ee2591bae3dcb017aa51
Piotrek Okoński <piotrek@okonski.org>
2014-07-22 22:58:51 +0200

    Add () and [] as word breakers.

--- lib/default_word_breaker.rb
+++ lib/default_word_breaker.rb
@@ -1,5 +1,5 @@
 class DefaultWordBreaker
-  BREAKERS = [" ", "\n", "\t", ".", ",", ";", ":"]
+  BREAKERS = [" ", "\n", "\t", ".", ",", ";", ":", "\\(", "\\)", "\\[", "\\]"]
 
   def initialize(source)
     @source = source
