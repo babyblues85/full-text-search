f9fcd297009ed8f37ad9ac13ff70e6ceb14ff340
Piotrek Okoński <piotrek@okonski.org>
2014-07-23 11:55:56 +0200

    Add tests for Thing model.

--- spec/models/thing_spec.rb
+++ spec/models/thing_spec.rb
@@ -1,5 +1,13 @@
-require 'rails_helper'
+describe Thing, type: :model do
+  it "has :content searchable" do
+    expect(Thing.searchable_columns_value).to eq([:content])
+  end
 
-RSpec.describe Thing, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "uses default English stemmer" do
+    expect(Thing.stemmer_value).to eq(DefaultStemmer)
+  end
+
+  it "uses default English word breaker" do
+    expect(Thing.word_breaker_value).to eq(DefaultWordBreaker)
+  end
 end
